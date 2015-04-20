f19dc7bd05ba28d1812e0f1183ab43453a5f6df4
query-string <atimofeev@reactant.ru>
2015-04-20 23:00:02 +0700

    Describe syntax changes in specs.

--- spec/piu_piu_spec.rb
+++ spec/piu_piu_spec.rb
@@ -1,11 +1,19 @@
 require "piu_piu"
 
 describe PiuPiu do
-  it "returns sharps if message is empty" do
-    expect(piupiu).to eql("###")
+  context "#piupiu_devide" do
+    it "without arguments returns sharps" do
+      expect(piupiu_devide).to eql("###")
+    end
   end
 
-  it "returns wrapped message if message is defined" do
-    expect(piupiu("Miu")).to eql("### Miu ###")
+  context "#piupiu_wrap" do
+    it "without arguments returns devider" do
+      expect(piupiu_wrap).to eql("###")
+    end
+
+    it "without arguments returns wrapped message" do
+      expect(piupiu_wrap "Meow!").to eql("### Meow! ###")
+    end
   end
 end
