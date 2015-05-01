7acf0427b73b6f36c93f631f02c56d006ccf48e1
query-string <atimofeev@reactant.ru>
2015-05-01 11:00:24 +0700

    Move Code Climate instance to the very first line.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
-require "piu_piu"
+ENV['CODECLIMATE_REPO_TOKEN'] = "30676ff831772153d0238cd5ccbab7b8044a17d57c2878b07aa04904e7246328"
 require "codeclimate-test-reporter"
-
 CodeClimate::TestReporter.start
+
+require "piu_piu"
 
 RSpec.configure do |config|
   # Use color in STDOUT
