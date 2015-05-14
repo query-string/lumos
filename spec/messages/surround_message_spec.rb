babf091ede43cb3bd6811059b027377a25fe680d
query-string <atimofeev@reactant.ru>
2015-05-14 14:54:02 +0700

    Fix surround spec (in the right place) :P

--- spec/messages/surround_message_spec.rb
+++ spec/messages/surround_message_spec.rb
@@ -3,11 +3,11 @@ require "spec_helper"
 describe Lumos::SurroundMessage do
 
   it "returns message" do
-    expect(described_class.new(message: "Defodio").message).to eq("Defodio")
+    expect(described_class.new(message: "Defodio").message).to eq("###########\n#         #\n# Defodio #\n#         #\n###########")
   end
 
   it "returns message" do
-    expect(described_class.new(message: "Defodio").message_line).to eq("###########\n#         #\n# Defodio #\n#         #\n###########")
+    expect(described_class.new(message: "Defodio").message_line).to eq("###########")
   end
 
 end
