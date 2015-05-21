20c733965254aa8eb44a620f82fad317e6326926
query-string <atimofeev@reactant.ru>
2015-05-21 12:55:25 +0700

    Use loop for including formatters.

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 - [x] Test to-do markdown
 
 ## Next
-- [ ] Take a close look at Lumos::Wrapper design (and naming)
+- [x] Take a close look at Lumos::Wrapper design (and naming)
 - [ ] Lumos::Wrapper might keep messages shortcuts (i.e. self.surround_message)
 - [ ] Use single method rather than `lumos_wrap` and `lumos_devide`
 - [ ] Rewrite README
--- lib/lumos.rb
+++ lib/lumos.rb
@@ -1,14 +1,8 @@
 require "active_support/core_ext/string"
 
-# @TODO.md: Require in loop
-require "lumos/formatters/base"
-require "lumos/formatters/surround"
-require "lumos/formatters/left"
-require "lumos/formatters/right"
-require "lumos/formatters/top"
-require "lumos/formatters/bottom"
-require "lumos/formatters/horizontal"
-require "lumos/formatters/vertical"
+%w(base surround left right top bottom horizontal vertical).each do |file|
+  require File.dirname(__FILE__) + "/lumos/formatters/#{file}"
+end
 
 require "lumos/wrapper"
 require "lumos/core_ext/kernel"
