f87bb7f560c9a16764780a8164edee56092a02d4
query-string <atimofeev@reactant.ru>
2015-06-20 07:10:22 +0700

    Fix broken specs.

--- spec/core_ext/kernel_spec.rb
+++ spec/core_ext/kernel_spec.rb
@@ -5,58 +5,58 @@ require "set"
 RSpec.describe "Lumos kernel extensions" do
   context "divider" do
     it "returns sharps if no arguments defined" do
-      expect { lumos }.to output("###").to_stdout
+      expect { lumos }.to output("###\n").to_stdout
     end
 
     it "returns sharps if only pointer defined" do
-      expect { lumos :> }.to output("###").to_stdout
+      expect { lumos :> }.to output("###\n").to_stdout
     end
 
     it "returns delimiter based string" do
-      expect { lumos :>, "-" }.to output("---").to_stdout
+      expect { lumos :>, "-" }.to output("---\n").to_stdout
     end
 
     it "returns delimiter based string repeated 10 times" do
-      expect { lumos :>, "@", 10 }.to output("@@@@@@@@@@").to_stdout
+      expect { lumos :>, "@", 10 }.to output("@@@@@@@@@@\n").to_stdout
     end
   end
 
   context "wrapper" do
     it "returns wrapped messages if no arguments except message defined" do
-      expect { lumos "Accio" }.to output("#########\n#       #\n# Accio #\n#       #\n#########").to_stdout
+      expect { lumos "Accio" }.to output("#########\n#       #\n# Accio #\n#       #\n#########\n").to_stdout
     end
 
     it "returns bottom positioned message" do
-      expect { lumos "Accio", {position: :bottom, delimiter: "@"} }.to output("Accio\n@@@@@").to_stdout
+      expect { lumos "Accio", {position: :bottom, delimiter: "@"} }.to output("Accio\n@@@@@\n").to_stdout
     end
   end
 
   context "objects" do
     it "returns wra pped Array" do
-      expect { lumos %w(One Two Freddy is coming for you Three Four better lock your door ) }.to output("##########################################################################\n#                                                                        #\n# [\"One\", \"Two\", \"Freddy\", \"is\", \"coming\", \"for\", \"you\", \"Three\", \"Four\" #\n# , \"better\", \"lock\", \"your\", \"door\"]                                    #\n#                                                                        #\n##########################################################################").to_stdout
+      expect { lumos %w(One Two Freddy is coming for you Three Four better lock your door ) }.to output("##########################################################################\n#                                                                        #\n# [\"One\", \"Two\", \"Freddy\", \"is\", \"coming\", \"for\", \"you\", \"Three\", \"Four\" #\n# , \"better\", \"lock\", \"your\", \"door\"]                                    #\n#                                                                        #\n##########################################################################\n").to_stdout
     end
 
     it "returns wrapped Hash" do
       domains = {ru: "Russia", th: "Thailand", "com.au" => "Australia", ph: "Philippines"}
-      expect { lumos domains, {length: 56}}.to output("############################################################\n#                                                          #\n# {:ru=>\"Russia\", :th=>\"Thailand\", \"com.au\"=>\"Australia\",  #\n# :ph=>\"Philippines\"}                                      #\n#                                                          #\n############################################################").to_stdout
+      expect { lumos domains, {length: 56}}.to output("############################################################\n#                                                          #\n# {:ru=>\"Russia\", :th=>\"Thailand\", \"com.au\"=>\"Australia\",  #\n# :ph=>\"Philippines\"}                                      #\n#                                                          #\n############################################################\n").to_stdout
     end
 
     it "returns wrapped Set" do
-      expect { lumos Set.new([1,2,3])}.to output("#####################\n#                   #\n# #<Set: {1, 2, 3}> #\n#                   #\n#####################").to_stdout
+      expect { lumos Set.new([1,2,3])}.to output("#####################\n#                   #\n# #<Set: {1, 2, 3}> #\n#                   #\n#####################\n").to_stdout
     end
 
     it "returns wrapped Struct" do
       Struct.new("Customer", :name, :address)
-      expect { lumos Struct::Customer.new("Dave", "123 Main")}.to output("##############################################################\n#                                                            #\n# #<struct Struct::Customer name=\"Dave\", address=\"123 Main\"> #\n#                                                            #\n##############################################################").to_stdout
+      expect { lumos Struct::Customer.new("Dave", "123 Main")}.to output("##############################################################\n#                                                            #\n# #<struct Struct::Customer name=\"Dave\", address=\"123 Main\"> #\n#                                                            #\n##############################################################\n").to_stdout
     end
 
     it "returns wrapped OpenStruct" do
-      expect { lumos OpenStruct.new(country: "Russia", population: 143_975_923)}.to output("########################################################\n#                                                      #\n# #<OpenStruct country=\"Russia\", population=143975923> #\n#                                                      #\n########################################################").to_stdout
+      expect { lumos OpenStruct.new(country: "Russia", population: 143_975_923)}.to output("########################################################\n#                                                      #\n# #<OpenStruct country=\"Russia\", population=143975923> #\n#                                                      #\n########################################################\n").to_stdout
     end
 
     it "returns wrapped ActiveRecord" do
       Struct.new("Customer", :name, :address)
-      expect { lumos Coffee.new(sort: "Cappuccino", price: 105, saturation: 25)}.to output("#####################################################################\n#                                                                   #\n# #<Coffee id: nil, sort: \"Cappuccino\", price: 105, saturation: 25> #\n#                                                                   #\n#####################################################################").to_stdout
+      expect { lumos Coffee.new(sort: "Cappuccino", price: 105, saturation: 25)}.to output("#####################################################################\n#                                                                   #\n# #<Coffee id: nil, sort: \"Cappuccino\", price: 105, saturation: 25> #\n#                                                                   #\n#####################################################################\n").to_stdout
     end
   end
 end
