5e43d9a2e27cf983b5ff558e7468fb76cb4847bd
query-string <atimofeev@reactant.ru>
2015-06-25 08:34:43 +0700

    Use direct method call rather than Formatter class in form Bottom spec.

--- spec/formatters/bottom_spec.rb
+++ spec/formatters/bottom_spec.rb
@@ -4,39 +4,39 @@ require "spec_helper"
 describe Lumos::Formatters::Bottom do
   context "messages" do
     it "returns wrapped message" do
-      expect(described_class.new(message: "Langlock").result).to eq("Langlock\n########")
+      expect { lumos "Langlock", position: :bottom}.to output("Langlock\n########\n").to_stdout
     end
 
     it "returns multiline message" do
-      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.").result).to eq("Coffee has long had a reputation as being unhealthy. But in almost eve\nry single respect that reputation is backward. The potential health be\nnefits are surprisingly large.\n######################################################################")
+      expect { lumos "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", position: :bottom}.to output("Coffee has long had a reputation as being unhealthy. But in almost eve\nry single respect that reputation is backward. The potential health be\nnefits are surprisingly large.\n######################################################################\n").to_stdout
     end
 
     it "returns multiline message with defined length" do
-      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", length: 35).result).to eq("Coffee has long had a reputation as\n being unhealthy. But in almost eve\nry single respect that reputation i\ns backward. The potential health be\nnefits are surprisingly large.\n###################################")
+      expect { lumos "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", position: :bottom, length: 35}.to output("Coffee has long had a reputation as\n being unhealthy. But in almost eve\nry single respect that reputation i\ns backward. The potential health be\nnefits are surprisingly large.\n###################################\n").to_stdout
     end
   end
 
   context "paddings" do
     it "returns zero-padding message" do
-      expect(described_class.new(message: "Levicorpus", padding: 0).result).to eq("Levicorpus\n##########")
+      expect { lumos "Langlock", position: :bottom, padding: 0}.to output("Langlock\n########\n").to_stdout
     end
 
     it "returns 1-digit padding message" do
-      expect(described_class.new(message: "Levicorpus", padding: 1).result).to eq("Levicorpus\n\n##########")
+      expect { lumos "Langlock", position: :bottom, padding: 1}.to output("Langlock\n\n########\n").to_stdout
     end
 
     it "returns 2-digits padding message" do
-      expect(described_class.new(message: "Levicorpus", padding: 2).result).to eq("Levicorpus\n\n\n##########")
+      expect { lumos "Langlock", position: :bottom, padding: 2}.to output("Langlock\n\n\n########\n").to_stdout
     end
   end
 
   context "delimiters" do
     it "returns delimiter message" do
-      expect(described_class.new(message: "Legilimens", delimiter: "☯").result).to eq("Legilimens\n☯☯☯☯☯☯☯☯☯☯")
+      expect { lumos "Legilimens", position: :bottom, delimiter: "☯"}.to output("Legilimens\n☯☯☯☯☯☯☯☯☯☯\n").to_stdout
     end
 
     it "returns message with multichar delimiter" do
-      expect(described_class.new(message: "Legilimens", delimiter: "=->").result).to eq("Legilimens\n=->=->=->=")
+      expect { lumos "Legilimens", position: :bottom, delimiter: "=->"}.to output("Legilimens\n=->=->=->=\n").to_stdout
     end
   end
 end
