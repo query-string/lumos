76a62496efdc6d04b63d76104df0a0af09c9bca2
query-string <atimofeev@reactant.ru>
2015-06-29 17:40:23 +0700

    Use direct method call rather than Formatter class in Surround spec.

--- spec/formatters/surround_spec.rb
+++ spec/formatters/surround_spec.rb
@@ -4,39 +4,39 @@ require "spec_helper"
 describe Lumos::Formatters::Surround do
   context "messages" do
     it "returns wrapped message" do
-      expect(described_class.new(message: "Defodio").result).to eq("###########\n#         #\n# Defodio #\n#         #\n###########")
+      expect { lumos "Defodio" }.to output("###########\n#         #\n# Defodio #\n#         #\n###########\n").to_stdout
     end
 
     it "returns multiline message" do
-      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.").result).to eq("##########################################################################\n#                                                                        #\n# Coffee has long had a reputation as being unhealthy. But in almost eve #\n# ry single respect that reputation is backward. The potential health be #\n# nefits are surprisingly large.                                         #\n#                                                                        #\n##########################################################################")
+      expect { lumos "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large." }.to output("##########################################################################\n#                                                                        #\n# Coffee has long had a reputation as being unhealthy. But in almost eve #\n# ry single respect that reputation is backward. The potential health be #\n# nefits are surprisingly large.                                         #\n#                                                                        #\n##########################################################################\n").to_stdout
     end
 
     it "returns multiline message with defined length" do
-      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", length: 35).result).to eq("#######################################\n#                                     #\n# Coffee has long had a reputation as #\n#  being unhealthy. But in almost eve #\n# ry single respect that reputation i #\n# s backward. The potential health be #\n# nefits are surprisingly large.      #\n#                                     #\n#######################################")
+      expect { lumos "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", length: 35 }.to output("#######################################\n#                                     #\n# Coffee has long had a reputation as #\n#  being unhealthy. But in almost eve #\n# ry single respect that reputation i #\n# s backward. The potential health be #\n# nefits are surprisingly large.      #\n#                                     #\n#######################################\n").to_stdout
     end
   end
 
   context "paddings" do
     it "returns zero-digit padding message" do
-      expect(described_class.new(message: "Densaugeo", padding: 0).result).to eq("###########\n#Densaugeo#\n###########")
+      expect { lumos "Densaugeo", padding: 0 }.to output("###########\n#Densaugeo#\n###########\n").to_stdout
     end
 
     it "returns 1-digit padding message" do
-      expect(described_class.new(message: "Densaugeo", padding: 1).result).to eq("#############\n#           #\n# Densaugeo #\n#           #\n#############")
+      expect { lumos "Densaugeo", padding: 1 }.to output("#############\n#           #\n# Densaugeo #\n#           #\n#############\n").to_stdout
     end
 
     it "returns 2-digits padding message" do
-      expect(described_class.new(message: "Densaugeo", padding: 2).result).to eq("###############\n#             #\n#             #\n#  Densaugeo  #\n#             #\n#             #\n###############")
+      expect { lumos "Densaugeo", padding: 2 }.to output("###############\n#             #\n#             #\n#  Densaugeo  #\n#             #\n#             #\n###############\n").to_stdout
     end
   end
 
   context "delimiters" do
     it "returns delimiter message" do
-      expect(described_class.new(message: "Deletrius", delimiter: "❤").result).to eq("❤❤❤❤❤❤❤❤❤❤❤❤❤\n❤           ❤\n❤ Deletrius ❤\n❤           ❤\n❤❤❤❤❤❤❤❤❤❤❤❤❤")
+      expect { lumos "Deletrius", delimiter: "❤" }.to output("❤❤❤❤❤❤❤❤❤❤❤❤❤\n❤           ❤\n❤ Deletrius ❤\n❤           ❤\n❤❤❤❤❤❤❤❤❤❤❤❤❤\n").to_stdout
     end
 
     it "returns message with multichar delimiter" do
-      expect(described_class.new(message: "Deletrius", delimiter: "=->").result).to eq("=->=->=->=->=->=-\n=->           =->\n=-> Deletrius =->\n=->           =->\n=->=->=->=->=->=-")
+      expect { lumos "Deletrius", delimiter: "=->" }.to output("=->=->=->=->=->=-\n=->           =->\n=-> Deletrius =->\n=->           =->\n=->=->=->=->=->=-\n").to_stdout
     end
   end
 end
