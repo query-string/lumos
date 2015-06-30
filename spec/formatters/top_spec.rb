0deb69afbabb22ee55772d7b35e2013fd89f6c02
query-string <atimofeev@reactant.ru>
2015-06-30 17:40:23 +0700

    Use direct method call rather than Formatter class in Top spec.

--- spec/formatters/top_spec.rb
+++ spec/formatters/top_spec.rb
@@ -4,39 +4,39 @@ require "spec_helper"
 describe Lumos::Formatters::Top do
   context "messages" do
     it "returns wrapped message" do
-      expect(described_class.new(message: "Immobulus").result).to eq("#########\nImmobulus")
+      expect { lumos "Immobulus", position: :top }.to output("#########\nImmobulus\n").to_stdout
     end
 
     it "returns multiline message" do
-      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.").result).to eq("######################################################################\nCoffee has long had a reputation as being unhealthy. But in almost eve\nry single respect that reputation is backward. The potential health be\nnefits are surprisingly large.")
+      expect { lumos "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", position: :top }.to output("######################################################################\nCoffee has long had a reputation as being unhealthy. But in almost eve\nry single respect that reputation is backward. The potential health be\nnefits are surprisingly large.\n").to_stdout
     end
 
     it "returns multiline message with defined length" do
-      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", length: 35).result).to eq("###################################\nCoffee has long had a reputation as\n being unhealthy. But in almost eve\nry single respect that reputation i\ns backward. The potential health be\nnefits are surprisingly large.")
+      expect { lumos "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", position: :top, length: 35 }.to output("###################################\nCoffee has long had a reputation as\n being unhealthy. But in almost eve\nry single respect that reputation i\ns backward. The potential health be\nnefits are surprisingly large.\n").to_stdout
     end
   end
 
   context "paddings" do
     it "returns zero-padding message" do
-      expect(described_class.new(message: "Imperio", padding: 0).result).to eq("#######\nImperio")
+      expect { lumos "Imperio", position: :top }.to output("#######\nImperio\n").to_stdout
     end
 
     it "returns 1-digit padding message" do
-      expect(described_class.new(message: "Imperio", padding: 1).result).to eq("#######\n\nImperio")
+      expect { lumos "Imperio", position: :top, padding: 1 }.to output("#######\n\nImperio\n").to_stdout
     end
 
     it "returns 2-digits padding message" do
-      expect(described_class.new(message: "Imperio", padding: 2).result).to eq("#######\n\n\nImperio")
+      expect { lumos "Imperio", position: :top, padding: 2 }.to output("#######\n\n\nImperio\n").to_stdout
     end
   end
 
   context "delimiters" do
     it "returns delimiter message" do
-      expect(described_class.new(message: "Impedimenta", delimiter: "★").result).to eq("★★★★★★★★★★★\nImpedimenta")
+      expect { lumos "Impedimenta", position: :top, delimiter: "★" }.to output("★★★★★★★★★★★\nImpedimenta\n").to_stdout
     end
 
     it "returns message with multichar delimiter" do
-      expect(described_class.new(message: "Impedimenta", delimiter: "=->").result).to eq("=->=->=->=-\nImpedimenta")
+      expect { lumos "Impedimenta", position: :top, delimiter: "=->" }.to output("=->=->=->=-\nImpedimenta\n").to_stdout
     end
   end
 end
