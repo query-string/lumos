0b7e2868f75d130a4d8cb443fa9e563b1d069362
query-string <atimofeev@reactant.ru>
2015-07-01 17:40:23 +0700

    Use direct method call rather than Formatter class in Vertical spec.

--- spec/formatters/vertical_spec.rb
+++ spec/formatters/vertical_spec.rb
@@ -4,33 +4,33 @@ require "spec_helper"
 describe Lumos::Formatters::Vertical do
   context "messages" do
     it "returns message" do
-      expect(described_class.new(message: "Obliviate").result).to eq("# Obliviate #")
+      expect { lumos "Obliviate", position: :vertical }.to output("# Obliviate #\n").to_stdout
     end
 
     it "returns multiline message" do
-      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.").result).to eq("# Coffee has long had a reputation as being unhealthy. But in almost eve #\n# ry single respect that reputation is backward. The potential health be #\n# nefits are surprisingly large.                                         #")
+      expect { lumos "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", position: :vertical }.to output("# Coffee has long had a reputation as being unhealthy. But in almost eve #\n# ry single respect that reputation is backward. The potential health be #\n# nefits are surprisingly large.                                         #\n").to_stdout
     end
 
     it "returns multiline message with defined length" do
-      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", length: 35).result).to eq("# Coffee has long had a reputation as #\n#  being unhealthy. But in almost eve #\n# ry single respect that reputation i #\n# s backward. The potential health be #\n# nefits are surprisingly large.      #")
+      expect { lumos "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", position: :vertical, length: 35 }.to output("# Coffee has long had a reputation as #\n#  being unhealthy. But in almost eve #\n# ry single respect that reputation i #\n# s backward. The potential health be #\n# nefits are surprisingly large.      #\n").to_stdout
     end
   end
 
   context "paddings" do
     it "returns zero-padding message" do
-      expect(described_class.new(message: "Oppugno", padding: 0).result).to eq("#Oppugno#")
+      expect { lumos "Oppugno", position: :vertical, padding: 0 }.to output("#Oppugno#\n").to_stdout
     end
 
     it "returns 1-digit padding message" do
-      expect(described_class.new(message: "Oppugno", padding: 1).result).to eq("# Oppugno #")
+      expect { lumos "Oppugno", position: :vertical, padding: 1 }.to output("# Oppugno #\n").to_stdout
     end
 
     it "returns 2-digits padding message" do
-      expect(described_class.new(message: "Oppugno", padding: 2).result).to eq("#  Oppugno  #")
+      expect { lumos "Oppugno", position: :vertical, padding: 2 }.to output("#  Oppugno  #\n").to_stdout
     end
   end
 
   it "returns delimiter message" do
-    expect(described_class.new(message: "Obscuro", delimiter: "☢").result).to eq("☢ Obscuro ☢")
+    expect { lumos "Obscuro", position: :vertical, delimiter: "☢" }.to output("☢ Obscuro ☢\n").to_stdout
   end
 end
