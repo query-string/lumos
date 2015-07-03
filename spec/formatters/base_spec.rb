03b0ddec8f7bc467ad927d2ed09e2a83387946d8
query-string <atimofeev@reactant.ru>
2015-07-03 10:07:14 +0700

    Fix Base spec.

--- spec/formatters/base_spec.rb
+++ spec/formatters/base_spec.rb
@@ -3,21 +3,23 @@ require "spec_helper"
 
 describe Lumos::Formatters::Base do
 
+  let(:options) { Lumos::Wrapper.default_options }
+
   context "subsidary methods" do
     it "returns number of message characters" do
       expect(described_class.new(message: "Cave Inimicum").message_length).to eq(13)
     end
 
     it "returns number of short message lines" do
-      expect(described_class.new(message: "Colloportus").chopped_message.size).to eq(1)
+      expect(described_class.new(message: "Colloportus", length: options[:length]).chopped_message.size).to eq(1)
     end
 
     it "returns number of long message lines" do
-      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.").chopped_message.size).to eq(3)
+      expect(described_class.new(message: "Coffee has long had a reputation as being unhealthy. But in almost every single respect that reputation is backward. The potential health benefits are surprisingly large.", length: options[:length]).chopped_message.size).to eq(3)
     end
 
     it "returns messages size based on lenght of first chopped line" do
-      expect(described_class.new(message: "Confringo").chopped_message_length).to eq(9)
+      expect(described_class.new(message: "Confringo", length: options[:length]).chopped_message_length).to eq(9)
     end
   end
 
